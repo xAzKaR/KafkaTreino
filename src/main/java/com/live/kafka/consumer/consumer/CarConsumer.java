e130e763169c0a835d41dc0c4db13783ab2ea4a5
Leandro Gomides <leandro.limacarneiro@nttdata.com>
2022-03-16 17:59:41 -0300

    consumer

---
+++ src/main/java/com/live/kafka/consumer/consumer/CarConsumer.java
@@ -0,0 +1,27 @@
+package com.live.kafka.consumer.consumer;
+
+import com.live.kafka.consumer.dto.CarDto;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CarConsumer {
+
+    private static final Logger log = LoggerFactory.getLogger(CarConsumer.class);
+
+    @Value(value = "${topic.name}")
+    private String topic;
+
+    @Value(value = "${spring.kafka.group-id}")
+    private String groupId;
+
+    @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.group-id}", containerFactory = "containerFactory")
+    public void listenTopicCar(ConsumerRecord<String, CarDto> record) {
+        log.info("Received Message" + record.partition());
+        log.info("Received Message" + record.value());
+    }
+}
